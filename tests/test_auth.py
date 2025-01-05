28611d8fd738f1721bcedb024a4c730539b53ecc
Kamaal Farah <kamaal.f1@gmail.com>
2025-01-05 15:13:04 +0000

    Testing register deterministically

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -61,19 +61,38 @@ def test_deterministic_login(
     )
 
 
-def test_register(client, database):
-    register_response = client.post(
-        "/auth/register",
-        data={"email": "yuno@golden.io", "password": "password-to-keep"},
+@responses.activate
+def test_deterministic_register(
+    client, database, freeze_test_time, deterministic_random
+):
+    responses.add(
+        responses.GET,
+        "https://jsonplaceholder.typicode.com/users",
+        json=[USER],
+        status=200,
     )
+
+    payload = {"email": "yuno@golden.io", "password": "password-to-keep"}
+    register_response = client.post("/auth/register", data=payload)
     json_response = register_response.json()
 
     assert register_response.status_code == HTTPStatus.CREATED
     assert json_response["details"] == "Created"
 
     with Session(database.engine) as session:
-        user = User.get_by_email(email="yuno@golden.io", session=session)
+        user = User.get_by_email(email=payload["email"], session=session)
 
         assert user is not None
-        assert user.email == "yuno@golden.io"
-        assert user.verify_password(raw_password="password-to-keep")
+        assert user.email == payload["email"]
+        assert user.verify_password(raw_password=payload["password"])
+
+    login_response = client.post("/auth/login", data=payload)
+    json_response = login_response.json()
+
+    assert login_response.status_code == HTTPStatus.OK
+    assert json_response["random"] == 0.41661987254534116
+    assert json_response["token_type"] == "bearer"
+    assert (
+        json_response["access_token"]
+        == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImV4cCI6MTczNTcyNzQwMH0.Jku3-3o_7x_pLSUrIxMwODMjhPb2DJWtop1RtW3JsYc"
+    )
