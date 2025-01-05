82949d4f766d94a83f1de6617401bdcfe9d7fbff
Kamaal Farah <kamaal.f1@gmail.com>
2025-01-05 13:57:27 +0000

    Deterministically testing login

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import random
 import uuid
 from pathlib import Path
 
 import pytest
 from fastapi.testclient import TestClient
+from freezegun import freeze_time
 from sqlalchemy import create_engine
+from sqlmodel import Session, select
 
+from dst_py.auth.models import User
+from dst_py.auth.schemas import UserSchema
 from dst_py.database import BaseDatabase, get_database
 from dst_py.main import app
 
@@ -14,24 +19,55 @@ class DatabaseForTests(BaseDatabase):
         super().__init__(create_engine(database_name, echo=False))
 
 
-def get_database_override(database_name: str):
-    database = DatabaseForTests(database_name)
-    database.create_db_and_tables()
-
+def get_database_override(database: DatabaseForTests):
     def override():
         yield database
 
     return override
 
 
-@pytest.fixture(scope="function")
-def client():
-    __client = TestClient(app)
+@pytest.fixture
+def database():
     database_filename = f"{uuid.uuid4()}.db"
-    database_name = f"sqlite:///{database_filename}"
-    app.dependency_overrides[get_database] = get_database_override(database_name)
+    database = DatabaseForTests(f"sqlite:///{database_filename}")
+    database.create_db_and_tables()
 
     try:
-        yield __client
+        yield database
     finally:
         Path(database_filename).unlink()
+
+
+@pytest.fixture(scope="function")
+def client(database, create_default_user):
+    __client = TestClient(app)
+    app.dependency_overrides[get_database] = get_database_override(database)
+
+    yield __client
+
+
+@pytest.fixture
+def default_user_credentials():
+    yield UserSchema(email="yami@bulls.io", password="nice_password")
+
+
+@pytest.fixture
+def create_default_user(database, default_user_credentials):
+    with Session(database.engine) as session:
+        query = select(User).where(User.email == default_user_credentials.email)
+        if user := session.exec(query).first():
+            yield user
+
+        yield User.create(payload=default_user_credentials, session=session)
+
+
+@pytest.fixture
+def freeze_test_time():
+    with freeze_time("2025-01-01 10:00:00"):
+        yield
+
+
+@pytest.fixture
+def deterministic_random():
+    random.seed(12345)
+    yield
---
+++ tests/test_auth.py
@@ -0,0 +1,57 @@
+from http import HTTPStatus
+
+import responses
+
+USER = {
+    "address": {
+        "city": "Lebsackbury",
+        "geo": {"lat": "-38.2386", "lng": "57.2232"},
+        "street": "Kattie Turnpike",
+        "suite": "Suite 198",
+        "zipcode": "31428-2261",
+    },
+    "company": {
+        "bs": "target end-to-end models",
+        "catchPhrase": "Centralized empowering task-force",
+        "name": "Hoeger LLC",
+    },
+    "email": "Rey.Padberg@karina.biz",
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "phone": "024-648-3804",
+    "username": "Moriah.Stanton",
+    "website": "ambrose.net",
+}
+
+
+@responses.activate
+def test_deterministic_login(
+    client, freeze_test_time, deterministic_random, default_user_credentials
+):
+    responses.add(
+        responses.GET,
+        "https://jsonplaceholder.typicode.com/users",
+        json=[USER],
+        status=200,
+    )
+
+    login_payload = {
+        **default_user_credentials.model_dump(mode="python"),
+        "password": "can_be_anything_wrong",
+    }
+    login_response = client.post("/auth/login", data=login_payload)
+
+    assert login_response.status_code == HTTPStatus.UNAUTHORIZED
+
+    login_payload["password"] = default_user_credentials.password
+    login_response = client.post("/auth/login", data=login_payload)
+    json_response = login_response.json()
+
+    assert login_response.status_code == HTTPStatus.OK
+    assert len(json_response["external_data"]) == 1
+    assert json_response["external_data"][0] == USER
+    assert json_response["random"] == 0.41661987254534116
+    assert (
+        json_response["access_token"]
+        == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImV4cCI6MTczNTcyNzQwMH0.WB-Skcc2-Qjtmun2SftTdCeSpGmpZOHmXGfeAvapidA"
+    )
